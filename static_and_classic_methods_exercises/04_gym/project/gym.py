333af3a83bca4038776a10b02f8c7c632a37a730
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-11-15 19:42:04 +0200

    add lab tasks

--- static_and_classic_methods_exercises/04_gym/project/gym.py
+++ static_and_classic_methods_exercises/04_gym/project/gym.py
@@ -36,13 +36,15 @@ class Gym:
 
     def subscription_info(self, subscription_id: int):
         subscription = next((s for s in self.subscriptions if s.id == subscription_id), None)
-        customer = next((c for c in self.customers if c.id == subscription.customer_id), None)
-        trainer = next((t for t in self.trainers if t.id == subscription.trainer_id), None)
-        plan = next((p for p in self.plans if p.id == subscription.exercise_id), None)
-        equipment = next((e for e in self.equipment if e.id == plan.equipment_id), None)
+        if subscription:
+            customer = next((c for c in self.customers if c.id == subscription.customer_id), None)
+            trainer = next((t for t in self.trainers if t.id == subscription.trainer_id), None)
+            plan = next((p for p in self.plans if p.id == subscription.exercise_id), None)
+            if plan:
+                equipment = next((e for e in self.equipment if e.id == plan.equipment_id), None)
 
-        return "\n".join([subscription.__repr__(),
-                          customer.__repr__(),
-                          trainer.__repr__(),
-                          equipment.__repr__(),
-                          plan.__repr__()])
+                return "\n".join([subscription.__repr__(),
+                                  customer.__repr__(),
+                                  trainer.__repr__(),
+                                  equipment.__repr__(),
+                                  plan.__repr__()])

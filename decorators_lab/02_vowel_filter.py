532810fc05fe3f0e5129ad910ee044a771c5d88c
Boris Popov <32306686+bpopovgit@users.noreply.github.com>
2023-11-27 21:40:51 +0200

    add lab tasks

---
+++ decorators_lab/02_vowel_filter.py
@@ -0,0 +1,15 @@
+def vowel_filter(function):
+
+    def wrapper():
+        result = function()
+        vowels = [char for char in result if char.lower() in 'aeouyei']
+        return vowels
+
+    return wrapper
+
+
+@vowel_filter
+def get_letters():
+    return ["a", "b", "c", "d", "e"]
+
+print(get_letters())
